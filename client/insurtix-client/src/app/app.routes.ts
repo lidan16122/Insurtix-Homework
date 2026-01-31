c259d41d6d32af3915fdbc9bb273e7b364180123
Lidan Sassonker <lidan16122@gmail.com>
2026-01-31 18:19:20 +0200

    removed unused code, added models and routes, and books service

--- client/insurtix-client/src/app/app.routes.ts
+++ client/insurtix-client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'books', component: BooksComponent },
+  { path: '', redirectTo: 'books', pathMatch: 'full' }
+];
--- client/insurtix-client/src/app/app.ts
+++ client/insurtix-client/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BooksComponent } from './books/books.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BooksComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
