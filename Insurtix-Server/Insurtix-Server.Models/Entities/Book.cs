61b422f99674e08af2689108c4c6edcc8a754452
Lidan Sassonker <lidan16122@gmail.com>
2026-01-31 15:01:35 +0200

    created a new get books endpoint with DI

--- Insurtix-Server/Insurtix-Server.Models/Entities/Book.cs
+++ Insurtix-Server/Insurtix-Server.Models/Entities/Book.cs
@@ -8,7 +8,7 @@ namespace Insurtix_Server.Models.Entities
     {
         public string Category { get; set; }
         public string? Cover { get; set; } 
-        public int ISBN { get; set; }
+        public string ISBN { get; set; }
         public string Title { get; set; }
         public string Lang { get; set; } = "en"; 
         public List<string> Authors { get; set; } = new List<string>();
